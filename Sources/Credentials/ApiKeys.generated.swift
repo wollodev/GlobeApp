680e37526afb24a22827421ee9e6ca9eac63f574
wollo <me@kluth.co.uk>
2018-12-27 11:16:56 +0100

    .gitignore is now working

--- Sources/Credentials/ApiKeys.generated.swift
+++
@@ -1,8 +0,0 @@
-#error("Add GoogleMaps Api-Key to './env-vars.sh' and delete this line")
-
-public enum ApiKeys: String {
-    case googleMaps = "generated-during-build"
-    public var key: String {
-        return self.rawValue
-    }
-}
